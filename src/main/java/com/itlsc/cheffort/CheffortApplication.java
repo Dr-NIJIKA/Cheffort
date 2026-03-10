b1d11191fef03de35bdfc8b04255927e6ba5be67
Dr-NIJIKA <1464245694@qq.com>
2026-03-10 21:30:41 +0800

    搭建springboot+maven初始架构

---
+++ src/main/java/com/itlsc/cheffort/CheffortApplication.java
@@ -0,0 +1,13 @@
+package com.itlsc.cheffort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheffortApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheffortApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itlsc/cheffort/CheffortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itlsc.cheffort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheffortApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
