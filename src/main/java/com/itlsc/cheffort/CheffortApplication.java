e33be155bacfd53cd54aa9888fdb14071e787127
Dr-NIJIKA <1464245694@qq.com>
2026-03-23 21:14:56 +0800

    用户登录接口调通，增加注册功能

--- src/main/java/com/itlsc/cheffort/CheffortApplication.java
+++ src/main/java/com/itlsc/cheffort/CheffortApplication.java
@@ -1,9 +1,13 @@
 package com.itlsc.cheffort;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@MapperScan("com.itlsc.mapper")
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.itlsc"})
 public class CheffortApplication {
 
     public static void main(String[] args) {
